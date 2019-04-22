9c0355844a4eda7f603fd45d8c2a25ecbf083b6a
Jakub Kubasiak <xjk@pfa.dk>
2019-04-22 20:06:39 +0200

    font awesome

---
+++ Cube.Secure/Converters/BoolToFileIconConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Cube.Secure.Converters
+{
+    public class BoolToFileIconConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isDirectory = (bool) value;
+            return isDirectory ? "Folder" : "File";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Cube.Secure/Converters/FileSizeToStringConverter.cs
+++ Cube.Secure/Converters/FileSizeToStringConverter.cs
@@ -16,7 +16,7 @@ namespace Cube.Secure.Converters
             if (value != null)
             {
                 var size = (long)value;
-                return size > 0 ? (size / (1024 * 1024)).ToString() : string.Empty;
+                return this.BytesToString(size);
             }
 
             return string.Empty;
@@ -26,6 +26,17 @@ namespace Cube.Secure.Converters
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return null;
+        }
+
+        private string BytesToString(long byteCount)
+        {
+            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" }; //Longs run out around EB
+            if (byteCount == 0)
+                return string.Empty;
+            long bytes = Math.Abs(byteCount);
+            int place = System.Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024.0*1000.0)));
+            double num = Math.Round(bytes / Math.Pow(1024.0*1000.0, place), 1);
+            return (Math.Sign(byteCount) * num).ToString(CultureInfo.InvariantCulture) + suf[place];
         }
     }
 }
